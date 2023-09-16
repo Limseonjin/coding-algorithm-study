0530ee6ba94fd014a15c190eab3d5fdab02923f9
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-09-16 10:17:37 +0900

    [백준]새싹/브론즈5

---
+++ deepStep1/Sprout.java
@@ -0,0 +1,12 @@
+package deepStep1;
+
+public class Sprout {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
